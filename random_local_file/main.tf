489e3655c99f43c52485a7b6ca9741479fbbdeba
Kacper Dworakowski <kacper.dworakowski@icloud.com>
2022-08-03 11:58:34 +0200

    and so the crazy refactoring process sees the sunlight after some months in the dark!

--- random_local_file/main.tf
+++ random_local_file/main.tf
@@ -2,11 +2,12 @@ variable "id" {
     type = string
 }
 
-resource "random_" "default" {
+resource "random_id" "default" {
     byte_length = 8
 }
 resource "local_file" "task2-out" {
+    count = 3
     content  = templatefile("backends.tpl", {ip_addrs = ["0.0.0.0", "8.8.8.8"], port=8080})
-    filename = "${path.module}/backend-${random_.default.hex}"
+    filename = "${path.module}/backend-${random_id.default.hex}-${count.index}"
     file_permission = "0640"
 }
