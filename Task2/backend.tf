8f52ffe0c833de2e98a0269c813110473c0afda0
Kacper Dworakowski <kacper.dworakowski@icloud.com>
2022-08-03 12:35:12 +0200

    This should work until december 2013.

--- Task2/backend.tf
+++ Task2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "azurerm" {
         resource_group_name  = "tfstate"
-        storage_account_name = "tfstate3001"
+        storage_account_name = "tfstate8962"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
     }
